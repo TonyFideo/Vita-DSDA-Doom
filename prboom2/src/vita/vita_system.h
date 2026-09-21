4e67c475cd8b4941a56ce2fc31d81eeb4b62ae41
Tony Fideo <49733381+TonyFideo@users.noreply.github.com>
2026-09-21 04:18:49 -0300

    feat(vita): declare launcher IWAD selection API

--- prboom2/src/vita/vita_system.h
+++ prboom2/src/vita/vita_system.h
@@ -2,10 +2,17 @@
 #define DSDA_VITA_SYSTEM_H
 
 void Vita_InitFilesystem(void);
+void Vita_RefreshIWADs(void);
 
 const char *Vita_DataRoot(void);
 const char *Vita_TempDir(void);
 const char *Vita_LogPath(void);
+
+int Vita_IWADCount(void);
+const char *Vita_IWADPathAt(int index);
+const char *Vita_IWADNameAt(int index);
+int Vita_SelectedIWADIndex(void);
+void Vita_SelectIWAD(int index);
 
 /* Returned string belongs to the zone allocator and must be Z_Free'd. */
 char *Vita_FindAutoIWAD(void);
